c7f6245e99099c15b51caa5fa3852542ae4920c1
Nicole Holder <nholder06@gmail.com>
2020-03-21 15:35:44 -0500

    finished graded assignment.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,7 +23,18 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if(searchType == "all" || searchTerm == ""){
+            jobs =JobData.findAll();
+        }else{
+            jobs=JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        return "/search";
+    }
 }
